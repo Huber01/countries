fcaf7d484f8ce803d824e7d8c00f40c8e961df5d
Huber01 <agusjr@gmail.com>
2022-07-27 17:06:55 -0300

    added delete route for activities

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -16,4 +16,18 @@ router.get('/', async(req,res,next)=>{
 
 })
 
+router.delete('/delete/:id', async(req,res,next)=>{
+	let {id} = req.params
+	try {
+		await Activity.destroy({where:{id}})
+
+		res.send('activity deleted')
+		
+	} catch (error) {
+		next(error)
+	}
+
+})
+
+
 module.exports = router
